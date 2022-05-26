093c37f003c5ea429b00f033f33fb26751711308
Monika Nawój <nawojmonika@gmail.com>
2022-05-26 10:13:48 +0200

    updated csv files to generate for every noise separately

--- calcNoise.py
+++ calcNoise.py
@@ -9,22 +9,22 @@ parser = argparse.ArgumentParser(description='calc noise')
 parser.add_argument('--name', default='MPRNet', type=str, help='Algorithm name')
 args = parser.parse_args()
 name = args.name
-out = '/content/output'
+input = '/content/output'
 
 noises = os.listdir('/content/denoise/input')
 images = ['barbara', 'boat', 'chronometer', 'lena', 'mandril', 'peppers']
 
-results = open(os.path.join(out+ '.csv'), 'w')
-writer = csv.writer(results)
 
 for noise in noises:
-    path = os.path.join(out, noise)
+    path = os.path.join(input, noise)
+    results = open(os.path.join(path + '.csv'), 'w')
+    writer = csv.writer(results)
 
     if not os.path.exists(path):
         os.makedirs(path)
 
     for image in images:
-        base = cv2.imread(os.path.join('/content/denoise/ground', image + '.pgm'), cv2.IMREAD_GRAYSCALE)
+        ground = cv2.imread(os.path.join('/content/denoise/ground', image + '.pgm'), cv2.IMREAD_GRAYSCALE)
         
         in_path = os.path.join('/content/denoise/input', noise, image + '.png')
         img = cv2.imread(in_path, cv2.IMREAD_GRAYSCALE)
@@ -32,7 +32,9 @@ for noise in noises:
         out_path = os.path.join(path, image + '.png')
         
         denoiser_img = cv2.imread(out_path, cv2.IMREAD_GRAYSCALE)
-        mse = mean_squared_error(base, denoiser_img)
-        psnr = cv2.PSNR(base, denoiser_img)
-        (ssim, diff) = structural_similarity(base, denoiser_img, full=True)
+        mse = mean_squared_error(ground, denoiser_img)
+        psnr = cv2.PSNR(ground, denoiser_img)
+        (ssim, diff) = structural_similarity(ground, denoiser_img, full=True)
         writer.writerow([round(mse, 3), round(psnr, 3), round(ssim, 3)])
+        
+        print(f"Results saved at {path}")
