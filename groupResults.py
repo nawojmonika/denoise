2c25c53a3d9b9a6075b4154ac901d8777264687f
Monika Nawój <nawojmonika@gmail.com>
2022-06-01 08:29:22 +0200

    Moved group results to repo

---
+++ groupResults.py
@@ -0,0 +1,23 @@
+import os
+import csv
+from natsort import natsorted
+from glob import glob
+
+captions = ['CycleISP-DND', 'CycleISP-SIDD', 'MPRNet-SIDD', 'Filtr dwustronny', 'Filtr Gaussowski', 'Filtr medianowy', 'Filtr Wienera']
+images = ['barbara', 'boat', 'chronometer', 'lena', 'mandril', 'peppers']
+
+noises = os.listdir('/content/output')
+for noise in noises:
+    path = os.path.join('/content/output', noise, '**', 'results.csv')
+    files = natsorted(glob(path, recursive=True))
+    for i, image in enumerate(images):
+      path = os.path.join('/content/output', noise, image + '.csv')
+      results = open(path, 'w')
+      writer = csv.writer(results)
+      writer.writerow(['', 'MSE', 'PSNR', 'SSIM'])
+      for j, file in enumerate(files):
+         with open(file) as fd:
+          reader=csv.reader(fd)
+          mse, psnr, ssim = [row for idx, row in enumerate(reader) if idx == i + 1][0]
+          writer.writerow([captions[j], mse, psnr, ssim])
+          print(f"Results saved at {path}")
