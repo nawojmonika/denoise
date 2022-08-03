af092c60e16f2ac20ada26d906f9b91e37eb6c9e
nawojmonika <nawojmonika@gmail.com>
2022-08-03 11:00:41 +0200

    Update validate_SIDD.py

--- validate_SIDD.py
+++ validate_SIDD.py
@@ -1,34 +1,53 @@
-import numpy as np
+import cv2
 import os
-from tqdm import tqdm
+import csv
 import torch
+import numpy as np
 import scipy.io as sio
 import torchvision
-from filter import filters
-print(filters)
-# from utils.names import getTestDatasets, getDatasets, getNetworkNames
+from tqdm import tqdm
+from skimage.metrics import structural_similarity
+from utils.filters import getFilters
+from utils.names import getFilterName
 
-# def restore(noisy, gt):
-
+filters = getFilters()
+path = '/content/validation/SIDD'
+results = open(os.path.join(path, 'results.csv'), 'w')
+writer = csv.writer(results)
+writer.writerow(['', 'PSNR', 'SSIM'])
 
 # Process data
-# basepath = '/content/validation/SIDD'
-# noisy = sio.loadmat(os.path.join(basepath, 'ValidationNoisyBlocksSrgb.mat'))
-# gt = sio.loadmat(os.path.join(basepath, 'ValidationGtBlocksSrgb.mat'))
+basepath = '/content/validation/SIDD'
+noisy = sio.loadmat(os.path.join(basepath, 'ValidationNoisyBlocksSrgb.mat'))
+gt = sio.loadmat(os.path.join(basepath, 'ValidationGtBlocksSrgb.mat'))
 
-# Inoisy = np.float32(np.array(noisy['ValidationNoisyBlocksSrgb']))
-# Inoisy /=255.
+Inoisy = np.float32(np.array(noisy['ValidationNoisyBlocksSrgb']))
+Inoisy /=255.
 
-# gt = np.float32(np.array(gt['ValidationGtBlocksSrgb']))
-# gt /=255.
+gt = np.float32(np.array(gt['ValidationGtBlocksSrgb']))
+gt /=255.
 
-# total_psnr = 0;
-# total_ssim = 0;
+for filter in filters:
+    total_psnr = 0;
+    total_ssim = 0;
+    name, add_filter = filter
 
-# with torch.no_grad():
-#     for i in tqdm(range(40)):
-#         for k in range(32):
-#             noisy_patch = torch.from_numpy(Inoisy[i,k,:,:,:]).unsqueeze(0).permute(0,3,1,2).cuda()
-#             noisy_img = torchvision.utils.make_grid(noisy_patch.cpu(), nrow=5).permute(1, 2, 0)
-#             gt_patch = torch.from_numpy(gt[i,k,:,:,:]).unsqueeze(0).permute(0,3,1,2).cuda()
-#             gt_img = torchvision.utils.make_grid(gt_patch.cpu(), nrow=5).permute(1, 2, 0)
+    with torch.no_grad():
+        for i in tqdm(range(40)):
+            for k in range(32):
+                noisy_patch = torch.from_numpy(Inoisy[i,k,:,:,:]).unsqueeze(0).permute(0,3,1,2).cuda()
+                noisy_img = torchvision.utils.make_grid(noisy_patch.cpu(), nrow=5).permute(1, 2, 0)
+                gt_patch = torch.from_numpy(gt[i,k,:,:,:]).unsqueeze(0).permute(0,3,1,2).cuda()
+                gt_img = torchvision.utils.make_grid(gt_patch.cpu(), nrow=5).permute(1, 2, 0)
+                
+                denoised_img = add_filter(noisy_img)
+                total_psnr += cv2.PSNR(gt_img, denoised_img)
+                (ssim, diff) = structural_similarity(gt_img, denoised_img, full=True, multichannel=True)
+                total_ssim +=ssim
+
+
+    qm_psnr = total_psnr / (40*32);
+    qm_ssim = total_ssim / (40*32);
+    writer.writerow([getFilterName(name), round(qm_psnr, 3), round(qm_ssim, 3)])
+
+print(f"Results saved at {path}/results.csv")
